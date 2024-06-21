3743a22263f7b9881759d2f9db3c62e517769024
owalkz <jjaraorie@gmail.com>
2024-06-21 09:54:10 +0300

    Added password hashing

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -1,4 +1,5 @@
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 
 exports.register = async (req, res, next) => {
     const {username, password} = req.body
@@ -6,15 +7,21 @@ exports.register = async (req, res, next) => {
         return res.status(400).json({message: 'Password must be at least 8 characters long'})
     }
     try {
-        await User.create({
-            username,
-            password,
-        }).then(user =>
-            res.status(200).json({
-                message: "User successfully created",
-                user,
-            })
-        )
+        bcrypt.hash(password, 10, async (err, hash) => {
+            if (err) {
+                return res.status(500).json({error: err.message})
+            }
+            await User.create({
+                username,
+                password: hash
+            }).then(user =>
+                res.status(200).json({
+                    message: "User successfully created",
+                    username: user.username,
+                    password: user.password
+                })
+            )
+        })
     } catch (err) {
         console.log(err);
         res.status(401).json({
