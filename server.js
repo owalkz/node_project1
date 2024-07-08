ef78092726bc52c4902cd8110672f0ab7224a951
owalkz <jjaraorie@gmail.com>
2024-07-08 14:45:06 +0300

    Modified Auth Routes

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 // const cookieParser = require("cookie-parser");
-// const corsOptions = {
-//   origin: "http://localhost:5173", // Your frontend URL
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-// };
+const corsOptions = {
+  origin: "http://localhost:5173", // Your frontend URL
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 // app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 const PORT = 5000;
 const connectDB = require("./db");
 connectDB();
